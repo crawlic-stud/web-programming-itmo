9fe4f302bdcd4844695358964614fea707acd298
crawlic-stud <nikitosik0726@gmail.com>
2023-10-22 03:01:18 +0300

    lab2 code is done

---
+++ lab2/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+
+
+function defaultTask(callback) {
+    console.log("hello world! it's gulp!");
+    callback();
+}
+
+
+gulp.task("count", function(callback) {
+    // пятый аргумент это будет аргумент после --num
+    var num = process.argv[4] ? process.argv[4] : 0;
+    if (num <= 0) {
+        console.log("Nothing to count");
+    } else {
+        for (var i = 1; i <= num; i++) {
+            console.log(i);
+        }
+    }
+    callback();
+})
+
+
+exports.default = defaultTask
